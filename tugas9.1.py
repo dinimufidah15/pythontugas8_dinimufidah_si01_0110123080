0ba37206f9a1c2233aa258c7514d00b9eeb1fde5
Dini Mufidah <146501788+dinimufidah15@users.noreply.github.com>
2023-11-25 21:06:54 +0700

    Add files via upload

---
+++ tugas9.1.py
@@ -0,0 +1,10 @@
+#deklarasi
+def profil(nama,alamat,gender,umur,hobby):
+    print("nama saya adalah",nama)
+    print("tempat tinggal di",alamat)
+    print("gender saya adalah",gender)
+    print("umur saya adalah",umur)
+    print("hobby saya adalah",hobby)
+#panggil
+profil("dini,cibinong,perempuan,19,tidur")
+
